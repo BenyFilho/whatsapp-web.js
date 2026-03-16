847af1cde5e29d0ec71413dd9d86fbffcbfb7769
BenyFilho <168232825+BenyFilho@users.noreply.github.com>
2026-03-16 20:40:21 -0300

    Update Message.js

--- src/structures/Message.js
+++ src/structures/Message.js
@@ -403,11 +403,12 @@ class Message extends Base {
     async getMentions() {
         return await Promise.all(
             this.mentionedIds.map(
-                async (m) => await this.client.getContactById(
-                    typeof m === 'string'
-                    ? m
-                    : m._serialized
-                ),
+                async (m) => 
+                    await this.client.getContactById(
+                        typeof m === 'string'
+                        ? m
+                        : m._serialized
+                    ),
             ),
         );
     }
